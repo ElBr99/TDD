cdc3a2314af0db819f717481073d7913af9360dd
Elizaveta Bredikhina <seepb99@mail.ru>
2023-04-11 20:22:51 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,8 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    Map<String, Long> map = new HashMap<>();
 
-
-    public int add (String name, long number) {
-        return 0;
+    public int add(String name, long number) {
+        map.put(name, number);
+        return map.size();
     }
 
 }
