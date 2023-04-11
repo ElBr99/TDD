24cfc7b2182eb8be85406864136089e07a78ff28
Elizaveta Bredikhina <seepb99@mail.ru>
2023-04-11 20:02:25 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,8 @@
 public class PhoneBook {
 
+
+    public int add (String name, long number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
 
+    @Test
+    void add() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        int resultFirst = phoneBook.add("Ivan", 89521458963L);
+        int resultSecond = phoneBook.add("Sveta", 89063415658L);
+        int resultThird = phoneBook.add("Sveta", 89063415658L);
+
+        Assertions.assertEquals(1, resultFirst);
+        Assertions.assertEquals(2, resultSecond);
+        Assertions.assertEquals(2, resultThird);
+    }
 }
